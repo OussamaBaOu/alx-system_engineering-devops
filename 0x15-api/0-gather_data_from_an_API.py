5e975fe10e4a2a6a0cc4c9be0b209dfa1f0751d9
O.B.O <125463724+OussamaBaOu@users.noreply.github.com>
2023-10-24 01:09:43 +0200

    p

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-'''Returns information about his/her TODO list progress.
+'''Returns information about his/her list progress.
 '''
 from requests import get
 from sys import argv
 
 if __name__ == '__main__':
-    main_url = 'https://jsonplaceholder.typicode.com'
+    main_url = 'https://jsonplaceholder.typicode.com/todos/1'
     todo_url = main_url + "/user/{}/todos".format(argv[1])
     name_url = main_url + "/users/{}".format(argv[1])
     todo_result = get(todo_url).json()
     name_result = get(name_url).json()
 
-    todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result
+    TOTAL_NUMBER_OF_TASKS = len(todo_result)
+    NUMBER_OF_DONE_TASKS = len([todo for todo in todo_result
                          if todo.get("completed")])
-    name = name_result.get("name")
+    EMPLOYEE_NAME = name_result.get("EMPLOYEE_NAME")
     print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_complete, todo_num))
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for todo in todo_result:
         if (todo.get("completed")):
             print("\t {}".format(todo.get("title")))
